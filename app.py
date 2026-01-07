868a8aad4fd997834caf9ecaafa432c41f0abd75
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2026-01-07 14:48:08 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,75 @@
+import sqlite3
+from flask import flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+DB_NAME = 'BooksDB.db'
+
+def connectDB():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = connectDB()
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS books (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        judul VARCHAR(100) NOT NULL,
+                        penulis VARCHAR(100) NOT NULL
+                    );''')
+    
+    conn.commit()
+    conn.close()
+
+@app.route('/')
+def index():
+    conn = connectDB()
+    books = conn.execute('SELECT * FROM books').fetchall()
+    conn.close()
+    return render_template('index.html', books=books)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        judul = request.form['judul']
+        penulis = request.form['penulis']
+        
+        conn = connectDB()
+        conn.execute('INSERT INTO books (judul, penulis) VALUES (?, ?)', (judul, penulis))
+        conn.commit()
+        conn.close()
+        
+        return redirect(url_for('index'))
+    
+    return render_template('add_book.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    conn = connectDB()
+    book = conn.execute('SELECT * FROM books WHERE id = ?', (id,)).fetchone()
+    if not book:
+        conn.close()
+        return 'Book not found', 404
+    
+    if request.method == 'POST':
+        judul = request.form['judul']
+        penulis = request.form['penulis']
+        conn.execute('UPDATE books SET judul = ?, penulis = ? WHERE id = ?', (judul, penulis, id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    
+    conn.close()
+    return render_template('edit_book.html', book=book)
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    conn = connectDB()
+    conn.execute('DELETE FROM books WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
